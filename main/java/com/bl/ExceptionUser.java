c848888a6f3a54bb6c2291b1aa9e116d2f08fb98
Your Name <you@example.com>
2022-03-16 21:58:14 +0530

    User Email Id

--- main/java/com/bl/ExceptionUser.java
+++ main/java/com/bl/ExceptionUser.java
@@ -5,7 +5,30 @@ import java.util.regex.Pattern;
 
 public class ExceptionUser 
 {
+	// validating first name
+			/**
+			 * created method fName and passing parameter in this it will check
+			 * FirstName is valid or not result will be in true or false because method is
+			 * boolean type
+			 * 
+			 * @param str -passing string of first name
+			 * @return -return to method created
+			 */
+			public boolean fName(String str) 
+			{
+				/**
+				 * The Matcher and Pattern classes provide the facility of Java regular
+				 * expression Pattern object represents a compile version of regular expression
+				 * we can create a pattern object by using compile() version of pattern class
+				 */
+				Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 
+				/**
+				 * we can use matcher object to match the given pattern in the target string we
+				 * can create matcher object by using matcher() of pattern class
+				 */
+				Matcher matcher = pattern.matcher(str);
+				return matcher.matches();}
 	/**
 	 * created method LName and passing parameter in this it will check FirstName
 	 * is valid or not result will be in true or false because method is boolean
@@ -30,4 +53,21 @@ public class ExceptionUser
 		Matcher matcher = pattern.matcher(str);
 		return matcher.matches();
 	}
-}
+		// validating email
+		/**
+		 * created method email and passing parameter in this it will check email is
+		 * valid or not result will be in true or false because method is boolean type
+		 * 
+		 * @param str -passing email id
+		 * @return -return to method created
+		 */
+		
+		public boolean email(String str) 
+		{
+			Pattern pattern = Pattern
+					.compile("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$");
+			Matcher matcher = pattern.matcher(str);
+			return matcher.matches();
+		}
+	
+	}

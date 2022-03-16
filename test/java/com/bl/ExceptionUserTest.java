c03f202efed25173a3a6f60aa41c302eabe2dd50
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-03-16 21:46:30 +0530

    Update ExceptionUserTest.java

--- test/java/com/bl/ExceptionUserTest.java
+++ test/java/com/bl/ExceptionUserTest.java
@@ -6,7 +6,27 @@ import org.junit.Test;
 public class ExceptionUserTest
 {
 	ExceptionUser exUS = new ExceptionUser();
+       @Test
+			/**
+			 * created method givenFirstName_IsProper_ReturnTrue() for true condition
+			 */
+			public void givenFName_IsProper_ReturnTrue() {
+				/**
+				 * calling fName method to see fName is valid or not
+				 */
+				boolean actualResult = exUS.fName("Saiprakash");
+				Assert.assertEquals(true, actualResult);
+			}
 
+			@Test
+			/**
+			 * created method givenFName_IsProper_ReturnFalse() for false condition
+			 */
+			public void givenFName_IsProper_ReturnFalse() 
+			{
+				boolean actualResult = exUS.fName("saiprakash");
+				Assert.assertEquals(false, actualResult);
+			}
 	@Test
 	/**
 	 * created method givenLastName_IsProper_ReturnTrue() for true condition
@@ -16,7 +36,7 @@ public class ExceptionUserTest
 		/**
 		 * calling LName method to see last name is valid or not
 		 */
-		boolean actualResult = exUS.LName("Advani");
+		boolean actualResult = exUS.LName("Beemari");
 		Assert.assertEquals(true, actualResult);
 	}
 
@@ -25,7 +45,7 @@ public class ExceptionUserTest
 	 * created method givenLName_IsProper_ReturnFalse() for false condition
 	 */
 	public void givenLName_IsProper_ReturnFalse() {
-		boolean actualResult = exUS.LName("advani");
+		boolean actualResult = exUS.LName("Beemari");
 		Assert.assertEquals(false, actualResult);
 	}
 }

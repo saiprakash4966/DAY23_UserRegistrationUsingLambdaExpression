b6b5f5c45ca6b58970992cda50e1abff1932fdeb
Your Name <you@example.com>
2022-03-16 21:41:34 +0530

    User Last Name

--- main/java/com/bl/ExceptionUser.java
+++ main/java/com/bl/ExceptionUser.java
@@ -5,32 +5,29 @@ import java.util.regex.Pattern;
 
 public class ExceptionUser 
 {
-	
-		// validating first name
+
+	/**
+	 * created method LName and passing parameter in this it will check FirstName
+	 * is valid or not result will be in true or false because method is boolean
+	 * type
+	 * 
+	 * @param str -passing string of last name
+	 * @return -return to method created
+	 */
+	public boolean LName(String str)
+	{
 		/**
-		 * created method fName and passing parameter in this it will check
-		 * FirstName is valid or not result will be in true or false because method is
-		 * boolean type
-		 * 
-		 * @param str -passing string of first name
-		 * @return -return to method created
+		 * The Matcher and Pattern classes provide the facility of Java regular
+		 * expression Pattern object represents a compile version of regular expression
+		 * we can create a pattern object by using compile() version of pattern class
 		 */
-		public boolean fName(String str) 
-		{
-			/**
-			 * The Matcher and Pattern classes provide the facility of Java regular
-			 * expression Pattern object represents a compile version of regular expression
-			 * we can create a pattern object by using compile() version of pattern class
-			 */
-			Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 
-			/**
-			 * we can use matcher object to match the given pattern in the target string we
-			 * can create matcher object by using matcher() of pattern class
-			 */
-			Matcher matcher = pattern.matcher(str);
-			return matcher.matches();
-		}
+		/**
+		 * we can use matcher object to match the given pattern in the target string we
+		 * can create matcher object by using matcher() of pattern class
+		 */
+		Matcher matcher = pattern.matcher(str);
+		return matcher.matches();
 	}
-
-
+}
--- test/java/com/bl/ExceptionUserTest.java
+++ test/java/com/bl/ExceptionUserTest.java
@@ -5,29 +5,27 @@ import org.junit.Test;
 
 public class ExceptionUserTest
 {
-	 		ExceptionUser exUS = new ExceptionUser();
+	ExceptionUser exUS = new ExceptionUser();
 
-			@Test
-			/**
-			 * created method givenFirstName_IsProper_ReturnTrue() for true condition
-			 */
-			public void givenFName_IsProper_ReturnTrue() {
-				/**
-				 * calling fName method to see fName is valid or not
-				 */
-				boolean actualResult = exUS.fName("Saiprakash");
-				Assert.assertEquals(true, actualResult);
-			}
+	@Test
+	/**
+	 * created method givenLastName_IsProper_ReturnTrue() for true condition
+	 */
+	public void givenLName_IsProper_ReturnTrue() 
+	{
+		/**
+		 * calling LName method to see last name is valid or not
+		 */
+		boolean actualResult = exUS.LName("Advani");
+		Assert.assertEquals(true, actualResult);
+	}
 
-			@Test
-			/**
-			 * created method givenFName_IsProper_ReturnFalse() for false condition
-			 */
-			public void givenFName_IsProper_ReturnFalse() 
-			{
-				boolean actualResult = exUS.fName("saiprakash");
-				Assert.assertEquals(false, actualResult);
-			}
-		}
-
-
+	@Test
+	/**
+	 * created method givenLName_IsProper_ReturnFalse() for false condition
+	 */
+	public void givenLName_IsProper_ReturnFalse() {
+		boolean actualResult = exUS.LName("advani");
+		Assert.assertEquals(false, actualResult);
+	}
+}

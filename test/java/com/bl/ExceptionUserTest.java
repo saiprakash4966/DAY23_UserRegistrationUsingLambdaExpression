7421d4ca1927733debacacc2351fab8a16e5a89b
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-03-17 17:59:01 +0530

    Update ExceptionUserTest.java

--- test/java/com/bl/ExceptionUserTest.java
+++ test/java/com/bl/ExceptionUserTest.java
@@ -110,7 +110,7 @@ public class ExceptionUserTest
 	/**
 	 * created method givePassword1_IsProper_ReturnTrue() for true condition
 	 */
-	public void givenPasswordRule2_IsProper_ReturnTrue() {
+	public void givenPassword1_IsProper_ReturnTrue() {
 		boolean actualResult = exUS.password1("Saiprakash");
 		Assert.assertEquals(true, actualResult);
 	}
